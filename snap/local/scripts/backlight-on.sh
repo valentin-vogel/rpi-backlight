e95c01b1aaf5cfbb027ccd01846c03c70a3c4e42
Valentin Vogel <vogelvali@gmail.com>
2021-04-27 01:01:59 +0200

    update

--- snap/local/scripts/backlight-on.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-echo -n 0 > /sys/class/backlight/rpi_backlight/bl_power
---
+++ snap/local/scripts/backlight.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+night_start="$(snapctl get night.start)"
+night_end="$(snapctl get night.end)"
+
+currenttime=$(date +%H:%M)
+
+if [[ "$currenttime" > "$night_start" ]] || [[ "$currenttime" < "$night_end" ]]; then
+    # light off
+    echo -n 1 > /sys/class/backlight/rpi_backlight/bl_power
+else
+    # light on
+    echo -n 0 > /sys/class/backlight/rpi_backlight/bl_power
+fi
